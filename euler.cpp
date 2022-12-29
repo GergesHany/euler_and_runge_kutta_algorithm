b482723d87c8e13f7e0f897158ce77b802806a97
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2022-12-29 12:47:19 +0200

    Update euler.cpp

--- euler.cpp
+++ euler.cpp
@@ -25,21 +25,6 @@ long double euler_method(double x0, double y0, double h, double x){
 }
 
 
-long double runge_kutta_method(double x0, double y0, double h, double x){
-   double y = y0;
-   while (x0 < x){
-      double k1 = h * equation(x0, y); 
-      double k2 = h * equation(x0 + 0.5 * h, y + 0.5 * k1); 
-      double k3 = h * equation(x0 + 0.5 * h, y + 0.5 * k2); 
-      double k4 = h * equation(x0 + h, y + k3); 
-      y = y + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4); 
-      x0 = x0 + h; 
-   }
-   return y;
-}
-
-// euler_and_runge_kutta_algorithm.cpp
-
 int main()
 {
   
@@ -47,7 +32,6 @@ int main()
 
    double x0 = 0, y0 = 1, h = 0.2, x = 0.4; // x is the value of x at which we want to find the value of y
    cout << euler_method(x0, y0, h, x) << "\n"; 
-   cout << runge_kutta_method(x0, y0, h, x) << "\n";
   
   return 0;
 }
