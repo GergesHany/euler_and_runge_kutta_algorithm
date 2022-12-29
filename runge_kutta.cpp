b433ec4cbc5a92cac91f34dcb5d9bd7be9458707
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2022-12-29 12:48:25 +0200

    Update and rename other.cpp to runge_kutta.cpp

--- runge_kutta.cpp
+++ runge_kutta.cpp
@@ -15,16 +15,6 @@ long double equation(double x, double y){
    return (x * x) + (y * y); // The equation
 }
 
-long double euler_method(double x0, double y0, double h, double x){
-   double y = y0;
-   while (x0 < x){
-      y = y + h * equation(x0, y); 
-      x0 = x0 + h; 
-   }
-   return y;
-}
-
-
 long double runge_kutta_method(double x0, double y0, double h, double x){
    double y = y0;
    while (x0 < x){
@@ -38,7 +28,6 @@ long double runge_kutta_method(double x0, double y0, double h, double x){
    return y;
 }
 
-// euler_and_runge_kutta_algorithm.cpp
 
 int main()
 {
@@ -46,7 +35,6 @@ int main()
    Gerges_Hany();
 
    double x0 = 0, y0 = 1, h = 0.2, x = 0.4; // x is the value of x at which we want to find the value of y
-   cout << euler_method(x0, y0, h, x) << "\n"; 
    cout << runge_kutta_method(x0, y0, h, x) << "\n";
   
   return 0;
